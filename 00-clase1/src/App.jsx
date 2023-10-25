2bd6260420413c8da19ebb6bc5ace4ddf15a6265
Facu Areosa <facundo.areosa@gmail.com>
2023-10-25 17:40:32 +0200

    fin clase 1

--- 00-clase1/src/App.jsx
+++ 00-clase1/src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
+import imgSisebuto from './assets/img/rey_sisebuto.png';
+import imgLeogivildo from './assets/img/rey_leogivildo.png';
+import imgAtanagildo from './assets/img/rey_atanagildo.png';
 
 function App() {
+  //ARRAYS INDIVIDUALES
+  let nombre=['Sisebuto','leogivildo','atanagildo'];
+  //PROBANDO OBJETOS
+  
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
-    </div>
+    <>
+    <section className="king">
+      <div className='king__div'>
+        <img src={imgSisebuto} className='king__img' />
+        <p className="king__name">{nombre[0]}</p>
+      </div>
+      <div className='king__div'>
+        <img src={imgLeogivildo} className='king__img' />
+        <p className="king__name">{nombre[1]}</p>
+      </div>
+      <div className='king__div'>
+        <img src={imgAtanagildo} className='king__img' />
+        <p className="king__name">{nombre[2]}</p>
+      </div>
+    </section>
+    </>
   )
 }
 
