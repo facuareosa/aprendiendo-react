38429acfc6a89dc66e899e2c64e0580557eac4e4
Facu Areosa <facundo.areosa@gmail.com>
2023-10-27 18:05:42 +0200

    fin clase 2

--- clase-02/src/App.jsx
+++ clase-02/src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import imagen0 from '../src/assets/images/rey_sisebuto.png'
+import imagen1 from '../src/assets/images/rey_teodorico.png'
+import {useRef} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const cambio = 23.16
+  const refCaja = useRef();
+  const incrementar = (e)=> {
+    e.target.innerHTML=Number(e.target.innerHTML) + 1
+    
+    if(e.target.innerHTML >= 10){
+      e.target.innerHTML = 1;
+    }
+    if(e.target.innerHTML>=8){
+      e.target.style.backgroundColor="red";
+    }else{
+      e.target.style.backgroundColor="white";
+    }
+  }
+  const convertir= ()=>{
+    refCaja.current.innerHTML=Number(refCaja.current.innerHTML)*cambio;
+  }
+  const cambiar = (e)=>{
+    if(e.target.src.includes("sisebuto")){
+      e.target.src = imagen1
+    }else{
+      e.target.src = imagen0
+    }
+  }
+  const lectura = (e)=>{
+    refCaja.current.innerHTML = e.target.value
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div ref={refCaja} className="caja" onClick={incrementar}>1</div>
+      <button onClick={convertir}>aceptar</button>
+      <div><img onClick={cambiar} src={imagen0} /></div>
+      <input onChange={lectura} className="campo" />
     </>
   )
 }
