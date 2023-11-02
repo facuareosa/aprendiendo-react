babb746e35b3a24441cd22f317e0883d4fa0e6f2
Facu Areosa <facundo.areosa@gmail.com>
2023-11-02 10:40:08 +0100

    actividad 1 codigo completado

---
+++ clase-05-act1/src/Components/Actividad1.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ReyCard({reyes}) {
+    const imgUrl = "http://www.html6.es/img/rey_"
+    const reyInfo = reyes.map((e,index) =>
+    <div key={index} className="card__container">
+        <p className="card__text">
+            <span className='card__nombre'>{e.nombre.toUpperCase()}</span> 
+            ha comido {e.reinado*e.vacasComidas*365} vacas en sus {e.reinado} años de reinado
+        </p>
+        <img src={imgUrl+e.nombre.toLowerCase()+".png"} alt="" />
+    </div>
+        )
+    return <>
+        {reyInfo}
+    </>;
+}
+
+export default ReyCard
+
