62c0fb6b1844638cb6c6e2a779831eaa08d8320d
Facu Areosa <facundo.areosa@gmail.com>
2023-11-08 19:05:41 +0100

    actividad clase 6 finalizada

--- clase-06/src/App.jsx
+++ clase-06/src/App.jsx
@@ -3,19 +3,42 @@ import './App.css'
 
 function App() {
 
-  const [joke,setJoke]=useState("");
+  const [joke,setJoke]=useState([]);
   
   const nextJoke =()=>{
-    const url = "https://api.chucknorris.io/jokes/random";
+    setJoke([])
+    const url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart&amount=10";
     const call = fetch(url);
     call
     .then(datos=>datos.json())
-    .then(object=>{setJoke(object.value)})
+    .then(object=>{
+      object.jokes.map((chistes)=>{
+        setJoke((e)=>
+        [...e,<div key={chistes.id}>
+          <p>{chistes.setup}</p>
+          <p>{chistes.delivery}</p>
+        </div>]
+        )
+      })
+    })
   }
   
   
 useEffect( () => {
-  nextJoke();
+  const url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart&amount=10";
+  const call = fetch(url);
+  call
+  .then(datos=>datos.json())
+  .then(object=>{
+    object.jokes.map((chistes)=>{
+      setJoke((e)=>
+      [...e,<div key={chistes.id}>
+        <p>{chistes.setup}</p>
+        <p>{chistes.delivery}</p>
+      </div>]
+      )
+    })
+  })
 },[]);
 
   return (
